2043f85633cec9e58179aebddddd691000a67496
sean marlow <smarlowucf@gmail.com>
2015-08-08 17:07:37 -0600

    add colorpicker

--- colorfield/fields.py
+++ colorfield/fields.py
@@ -9,13 +9,13 @@ from django.core.validators import RegexValidator
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
-color_re = re.compile('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
+color_re = re.compile('^rgba?\((?:\d{1,3}[,\)]){3}(?:\d+\.\d+\))$')
 validate_color = RegexValidator(color_re, _('Enter a valid color.'), 'invalid')
 
 
 class ColorWidget(forms.Widget):
     class Media:
-        js = [settings.STATIC_URL + 'colorfield/jscolor/jscolor.js']
+        js = [settings.STATIC_URL + 'colorfield/colorpicker/jqColorPicker.min.js']
         
     def render(self, name, value, attrs=None):
         return render_to_string('colorfield/color.html', locals())
